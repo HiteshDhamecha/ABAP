bc43b5bf7f45324ee838c4140841ad40c25b6cb4
Sanket <spatil@acep.org>
2025-02-28 14:29:37 +0530

    Initial commit installed memberjunction 2.26.1 successfully

---
+++ MJAPI/src/generated/generated.ts
@@ -0,0 +1,22 @@
+/********************************************************************************
+* ALL ENTITIES - TypeGraphQL Type Class Definition - AUTO GENERATED FILE
+* Generated Entities and Resolvers for Server
+*
+*   >>> DO NOT MODIFY THIS FILE!!!!!!!!!!!!
+*   >>> YOUR CHANGES WILL BE OVERWRITTEN
+*   >>> THE NEXT TIME THIS FILE IS GENERATED
+*
+**********************************************************************************/
+import { Arg, Ctx, Int, Query, Resolver, Field, Float, ObjectType, FieldResolver, Root, InputType, Mutation,
+            PubSub, PubSubEngine, ResolverBase, RunViewByIDInput, RunViewByNameInput, RunDynamicViewInput,
+            AppContext, KeyValuePairInput, DeleteOptionsInput, GraphQLTimestamp as Timestamp,
+            GetReadOnlyDataSource, GetReadWriteDataSource } from '@memberjunction/server';
+import { Metadata, EntityPermissionType, CompositeKey } from '@memberjunction/core'
+
+import { MaxLength } from 'class-validator';
+import { DataSource } from 'typeorm';
+import * as mj_core_schema_server_object_types from '@memberjunction/server'
+
+
+export {}
+    
---
+++ SQL Scripts/generated/et-ci-dev-abpa-sqldb/__mj/spDeleteAIAgent.sp.permissions.generated.sql
@@ -0,0 +1,3 @@
+
+GRANT EXECUTE ON [__mj].[spDeleteAIAgent] TO [cdp_Integration]
+
