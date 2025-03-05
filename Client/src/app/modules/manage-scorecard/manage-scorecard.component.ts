502901ab09ba33572a0095be400af9a1a3580cf3
Sanket <spatil@acep.org>
2025-03-05 16:02:34 +0530

    Added new sidebar item and minor style changes

---
+++ Client/src/app/modules/manage-scorecard/manage-scorecard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-scorecard',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-scorecard.component.html',
+  styleUrl: './manage-scorecard.component.css'
+})
+export class ManageScorecardComponent {
+
+}
