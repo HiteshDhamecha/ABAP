fb6441b07373e048f94872bd010bf8953b81d19c
Hitesh <hitesh.dhamecha@elastikteams.com>
2025-03-27 11:45:12 +0530

    Error Logging

--- Client/src/app/modules/user-abstract-form/user-abstract-form.component.ts
+++ Client/src/app/modules/user-abstract-form/user-abstract-form.component.ts
@@ -144,7 +144,8 @@ export class UserAbstractFormComponent implements OnInit {
         this.submittingForm = false;
         this.router.navigate(['view-details', this.eventID]);
       }else{
-        alert('Abstract form Not Submitted!');
+        
+        alert('Abstract form Not Submitted! '+abstractEntity.LatestResult);
       }
     };
 
