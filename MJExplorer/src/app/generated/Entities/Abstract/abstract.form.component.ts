6bff699712fa0eb0a618139f6d31d7a7a1f375d2
Hitesh <hitesh.dhamecha@elastikteams.com>
2025-03-28 20:09:29 +0530

    Teting MJExplorer

--- MJExplorer/src/app/generated/Entities/Abstract/abstract.form.component.ts
+++ MJExplorer/src/app/generated/Entities/Abstract/abstract.form.component.ts
@@ -4,6 +4,7 @@ import { RegisterClass } from '@memberjunction/global';
 import { BaseFormComponent } from '@memberjunction/ng-base-forms';
 import { LoadAbstractDetailsComponent } from "./sections/details.component"
 import { UserViewGridComponent } from "@memberjunction/ng-user-view-grid"
+import { Metadata } from '@memberjunction/core';
 
 @RegisterClass(BaseFormComponent, 'Abstracts') // Tell MemberJunction about this class
 @Component({
@@ -12,7 +13,25 @@ import { UserViewGridComponent } from "@memberjunction/ng-user-view-grid"
     styleUrls: ['../../../../shared/form-styles.css']
 })
 export class AbstractFormComponent extends BaseFormComponent {
+     md = new Metadata();
+  
+  
+
+async submittest() {
+    this.md = new Metadata();
+    const abstractEntity = await this.md.GetEntityObject<AbstractEntity>('Abstracts');
+    abstractEntity.SessionID = "58FCC61A-BF88-4E54-9F08-37F8EFD2BF58";
+    abstractEntity.UserID = "58FCC61A-BF88-4E54-9F08-37F8EFD2BF58";
+    if(await abstractEntity.Save()){
+      alert('Abstract form submitted!');
+    }else{
+      console.log("Error saving Abstarct ", abstractEntity.LatestResult)        
+      alert('Abstract form Not Submitted! ');
+    }
+}
     public record!: AbstractEntity;
+
+    
 } 
 
 export function LoadAbstractFormComponent() {
