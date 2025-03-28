98dfc7dd4b2c61af384ea8c3825bd18e3e907408
Hitesh <hitesh.dhamecha@elastikteams.com>
2025-03-28 12:56:00 +0530

    Trying with New Angular App

--- Client/README.md
+++ Client/README.md
@@ -1,57 +1,27 @@
-# Client/Client/README.md
+# SampleApp
 
-# Client Angular Application
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.7.
 
-This is the Client Angular application project.
+## Development server
 
-## Project Structure
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-- `src/app`: Contains the main application components and modules.
-- `src/assets`: Directory for static assets such as images and fonts.
-- `src/environments`: Contains environment-specific settings for development and production.
-- `src/index.html`: The main HTML file serving as the entry point for the application.
-- `src/main.ts`: The main entry point for bootstrapping the application.
-- `src/polyfills.ts`: Includes necessary polyfills for browser compatibility.
-- `src/styles.scss`: Global styles for the application.
+## Code scaffolding
 
-## Setup Instructions
-
-1. Clone the repository:
-   ```
-   git clone <repository-url>
-   ```
-
-2. Navigate to the project directory:
-   ```
-   cd Client
-   ```
-
-3. Install the dependencies:
-   ```
-   npm install
-   ```
-
-4. Run the application:
-   ```
-   ng serve
-   ```
-
-5. Open your browser and navigate to `http://localhost:4200` to see the application in action.
-
-## Testing
-
-To run the unit tests for the application, use the following command:
-```
-ng test
-```
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Build
 
-To build the application for production, use:
-```
-ng build --prod
-```
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## License
+## Running unit tests
 
-This project is licensed under the MIT License.
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService, IdToken, User } from '@auth0/auth0-angular';
 import { Metadata } from '@memberjunction/core';
 import { GraphQLDataProvider, GraphQLProviderConfigData, setupGraphQLClient } from '@memberjunction/graphql-dataprovider';
 import { environment } from 'src/environments/environment';
 import { LoginComponent } from './modules/login/login.component';
+import { UserService } from './service/user.service';
+import { MJAuthBase } from '@memberjunction/ng-auth-services';
 
+
+
+import { lastValueFrom } from 'rxjs';
+
+
+import { SharedService } from '@memberjunction/ng-shared';
 export type RefreshTokenFunction = () => Promise<string>;
 
 @Component({
@@ -15,10 +23,14 @@ export type RefreshTokenFunction = () => Promise<string>;
 })
 export class AppComponent implements OnInit {
   private loginComponent: LoginComponent | null = null;
-  private currentPathName: string;
+  public showSidebar: boolean = true;
 
-  constructor(private router: Router, public authService: AuthService) {
-    this.currentPathName = global.location.pathname;
+  constructor(private router: Router, public authService: AuthService, private userService: UserService, @Inject(MJAuthBase) public authBase: MJAuthBase) { 
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showSidebar = event.url !== '/';
+      }
+    });
   }
 
   public async ngOnInit(): Promise<void> {
@@ -56,56 +68,81 @@ export class AppComponent implements OnInit {
 
   private async SetupApp(token: string, user: User): Promise<void> {
     try {
-      if (!token) {
-        console.error("Unable to setup app: No token provided");
-        this.NavigateToLogin();
-        return;
+        if (!token) {
+            console.error("Unable to setup app: No token provided");
+            this.NavigateToLogin();
+            return;
+        }
+
+        const url: string = environment.GRAPHQL_URI;
+        const wsurl: string = environment.GRAPHQL_WS_URI;
+        const start = new Date();
+        const refreshTokenFunction: RefreshTokenFunction = async () => { return 'NoToken'; };
+        const config = new GraphQLProviderConfigData(token, url, wsurl, async () => {
+          const refresh$ = await this.authBase.refresh();
+          const claims = await lastValueFrom(refresh$);
+          const token = environment.AUTH_TYPE === 'auth0' ? claims?.__raw : claims?.idToken;
+          return token;
+        }, environment.MJ_CORE_SCHEMA_NAME);
+        const provider: GraphQLDataProvider = await setupGraphQLClient(config);
+
+        await SharedService.RefreshData(true);
+        
+        const md: Metadata = new Metadata();
+
+        if (!md.CurrentUser) {
+            console.error("User is logged in but no user found in metadata");
+            this.NavigateToLogin();
+            return;
+        }else{
+          // console.log(md.CurrentUser);
+          this.userService.setUserInfo(md.CurrentUser);
+        }
+
+        console.log("User Info:", md.CurrentUser);
+        
+        // Store user type in local storage or service
+        localStorage.setItem('userType', md.CurrentUser.Type.trim());
+        this.userService.setUserType(md.CurrentUser.Type.trim()); 
+
+
+        if (md.CurrentUser.Email !== user.email) {
+            await provider.Refresh();
+        }
+        else{
+          console.log("User's email matches metadata email");
+        }
+    
+        const end = new Date();
+        const elapsed = (end.getTime() - start.getTime()) / 1000;
+        console.log(`GraphQL client setup in ${elapsed} seconds`);
+
+        console.log("App setup complete");
+        localStorage.setItem(environment.TOKEN_CACHE_KEY, token);
+
+        this.loginComponent?.SetLoading(false);
+        if(md.CurrentUser.Type.trim()=='Owner'){
+          this.router.navigate(['/event-settings']);
+        }else{
+          this.router.navigate(['/view-event'])
+        }
+         
+
+    }    catch (err) {
+      if (err.response && err.response.errors) {
+        const unauthorizedError = err.response.errors.find((error: any) => error.extensions.code === 'UNAUTHORIZED');
+        if (unauthorizedError) {
+          console.error("Access denied! You don't have permission for this action!");
+          this.NavigateToLogin();
+          return;
+        }
       }
-      
-      const url: string = environment.GRAPHQL_URI;
-      const wsurl: string = environment.GRAPHQL_WS_URI;
-
-      const start = new Date();
-
-      const refreshTokenFunction: RefreshTokenFunction = async () => { return 'NoToken'; };
-      const config = new GraphQLProviderConfigData(token, url, wsurl, refreshTokenFunction);
-
-      const provider: GraphQLDataProvider = await setupGraphQLClient(config);
-
-      // Check to see if the user has access
-      const md: Metadata = new Metadata();
-      if (!md.CurrentUser){
-        console.error("User is logged in but no user found in metadata");
-        this.NavigateToLogin();
-        return;
-      }
-
-      if(md.CurrentUser.Email !== user.email){
-        console.log("User's email does not match metadata email, refreshing metadata");
-        await provider.Refresh();
-      }
-
-      const end = new Date();
-      const elapsed = (end.getTime() - start.getTime()) / 1000;
-      console.log(`GraphQL client setup in ${elapsed} seconds`);
-      console.log("App setup complete");
-      
-      localStorage.setItem(environment.TOKEN_CACHE_KEY, token);
-
-      this.loginComponent?.SetLoading(false);
-      if(this.currentPathName === '/'){
-        this.router.navigate(['/user-abstract-form']);
-      }
-      else{
-        this.router.navigate([this.currentPathName]);
-      }
-    } 
-    catch (err) {
       console.error("Error setting up app:");
       console.error(err);
       this.NavigateToLogin();
     }
-  }
+}
+
 
   private NavigateToLogin(): void {
     this.loginComponent?.SetLoading(false);
